26ad1ca4aba4c58075b5e1d2e7f04ca609fc787d
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-01-08 20:05:01 -0600

    post route to add product functions

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 // GET ROUTE
@@ -17,7 +17,7 @@ router.post('/', rejectUnauthenticated, (req, res) => {
     let queryText = `INSERT INTO "product" (name, description, size, cost, image_path)
                      VALUES ( $1, $2, $3, $4, $5 )`;
     pool.query(queryText, [req.body.name, req.body.description, req.body.size, 
-                           req.body.cost, req.body.imagee_path])
+                           req.body.cost, req.body.image_path])
         .then(() => res.sendStatus(201))
         .catch((error) => { 
           console.log('Bad news bears error in server POST adding product ---->', error)
